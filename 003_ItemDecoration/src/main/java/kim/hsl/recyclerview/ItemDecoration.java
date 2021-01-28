1aac3bb55e0a2fa4fbf298e46c736fade30a9cb7
han1202012 <octopus_truth@163.com>
2021-01-28 11:31:25 +0800

    update

---
+++ 003_ItemDecoration/src/main/java/kim/hsl/recyclerview/ItemDecoration.java
@@ -0,0 +1,27 @@
+package kim.hsl.recyclerview;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemDecoration extends RecyclerView.ItemDecoration {
+
+    @Override
+    public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent,
+                       @NonNull RecyclerView.State state) {
+    }
+
+    @Override
+    public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent,
+                           @NonNull RecyclerView.State state) {
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
+                               @NonNull RecyclerView parent,
+                               @NonNull RecyclerView.State state) {
+    }
+}
---
+++ 003_ItemDecoration/src/main/java/kim/hsl/recyclerview/MainActivity.java
@@ -0,0 +1,71 @@
+package kim.hsl.recyclerview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //1 . 从布局中获取 RecyclerView
+        RecyclerView recycler_view = findViewById(R.id.recycler_view);
+
+        //2 . 创建并设置布局管理器
+        //创建布局管理器
+
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(
+                4,
+                RecyclerView.VERTICAL);
+
+        //设置布局管理器
+        recycler_view.setLayoutManager(layoutManager);
+
+        //3 . 创建并设置列表适配器
+        Adapter adapter = new Adapter();
+        recycler_view.setAdapter(adapter);
+    }
+
+    /**
+     * RecyclerView 适配器
+     */
+    public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View root_view = LayoutInflater.from(MainActivity.this)
+                    .inflate(R.layout.item_recyclerview, parent, false);
+            return new ViewHolder(root_view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.text.setText("" + position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView text;
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                text = itemView.findViewById(R.id.text);
+            }
+        }
+    }
+
+}
